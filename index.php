559b27c2c143d0f07a53eb3e6fbea08cd22610ba
Kitenge Mukaba Jean-louis <101065560+Mukaba@users.noreply.github.com>
2022-11-28 22:01:43 +0000

    ***

--- index.php
+++ index.php
@@ -7,9 +7,6 @@
   <title>Jean-louis Mukaba</title>
   <link rel="shortcut icon" href="./assets/images/logo.ico" type="image/x-icon">
   <link rel="stylesheet" href="./assets/css/style.css">
-  <!--
-    - google font link
-  -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
